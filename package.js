9bb6df4bf6dff87636282a19e033fed1948198a2
Abigail Watson <awatson1978@gmail.com>
2015-11-04 01:57:07 -0800

    updated package.js dep

--- package.js
+++ package.js
@@ -27,7 +27,7 @@ Package.on_use(function (api) {
 });
 
 Package.on_test(function (api) {
-  api.use('iron:router-location');
+  api.use('clinical:router-location');
   api.use('tinytest');
   api.use('test-helpers');
 
